4b133e81ff1618a8f620c4d7957267ee38aaadd0
imharunguyen <thanh.big.boss21@gmail.com>
2022-05-13 18:41:55 +0700

    upload buoi1

---
+++ buoi1_4_23_22/homework1.py
@@ -0,0 +1,29 @@
+# -------------BAI_1
+a = input('Nhap ten cua ban: ')
+print('Xin chao ' + a)
+
+# -------------BAI_2
+a = float(input('Nhap so thu nhat: '))
+b = float(input('Nhap so thu hai: '))
+c = float(input('Nhap so thu ba: '))
+print((a + b + c) / 3)
+
+# -------------BAI_3
+h = float(input('Nhap chieu cao: '))
+w = float(input('Nhap can nang: '))
+print(round((w / h**2), 2))
+
+# -------------BAI_4
+a = int(input('Nhap he so a: '))
+b = int(input('Nhap b: '))
+c = int(input('nhap c: '))
+print((c - b)/a)
+
+#-------------BAI_5
+r = float(input('Nhap ban kinh: '))
+print(2 * r * 3.14)
+print(r**2 * 3.14)
+
+# -------------BAI_6
+n = int(input('nhap so nguyen duong n: '))
+print(int((n * (n + 1)) / 2))
