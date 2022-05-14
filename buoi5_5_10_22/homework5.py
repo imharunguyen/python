c1f1e6841cf01f45191b5cc4f3e1fd1e23d77207
Haru Nguyen <104016494+imharunguyen@users.noreply.github.com>
2022-05-14 17:24:12 +0700

    Update homework5.py

--- buoi5_5_10_22/homework5.py
+++ buoi5_5_10_22/homework5.py
@@ -1,36 +1,48 @@
 #github: https://github.com/imharunguyen/python-core
 
 #----Tìm max min và vị trí
-n = int(input('Nhap so so muon nhap: '))
-M = 0
-m = 0
-for i in range(n):
-    x = int(input())
-    if x > M:
-        M = x
-        global locate_M
-        locate_M = i
-    if x < m:
-        m = x
-        global locate_m
-        locate_m = i
+n = int(input('n = '))
+min_val, max_val = 0, 0
+min_val_ind, max_val_ind = 0, 0
 
-print('Min:', m, 'o v tri', locate_m)
-print('Max:', M, 'o vi tri', locate_M)
+for i in range(n):
+    temp = int(input())
+    if i:
+        if temp < min_val:
+            min_val = temp
+            min_val_ind = i
+        if temp > max_val:
+            max_val = temp
+            max_val_ind = i
+    else:
+        min_val = temp
+        min_val_ind = i
+
+        max_val = temp
+        max_val_ind = i
+
+print('Min:', min_val, 'o vi tri', min_val_ind)
+print('Max:', max_val, 'o vi tri', max_val_ind)
 
 #-------------tổng các số, số lẻ
-n = int(input('Nhap so so muon nhap: '))
-s_all = 0
-s_odd = 0
-all = ''
-odd = ''
-for i in range(n):
-    x = int(input())
-    s_all += x
-    all += str(x) + ' + '
-    if x % 2 != 0:
-        odd += str(x) + ' + '
-        s_odd += x
-print('Tong cac so la:    ', all, '=', s_all)
-print('Tong cac so le la: ', odd, '=', s_odd)
+n = int(input('n = '))
+even_sum = 0
+even_sum_str = ''
+odd_sum = 0
+odd_sum_str = ''
 
+for i in range(n):
+    temp = int(input())
+    if temp % 2 == 0:
+        even_sum += temp
+        if even_sum_str != '':
+            even_sum_str += ' + '
+        even_sum_str = f'{even_sum_str}{temp}'
+    else:
+        odd_sum += temp
+        if odd_sum_str != '':
+            odd_sum_str += ' + '
+        odd_sum_str = f'{odd_sum_str}{temp}'
+
+print(odd_sum_str, '=', odd_sum)
+print(even_sum_str, '=', even_sum)
